6f279703febef51bac44f1d6c4b44dd83ff67235
asanchy <sanchyclips@gmail.com>
2021-12-22 12:28:03 +0200

    Add html and javascript for mobile menu

---
+++ assets/js/menu.js
@@ -0,0 +1,12 @@
+const body = document.querySelector('body');
+const menu = document.querySelector('.menu-icon');
+
+menu.addEventListener('click', () => {
+  body.classList.add('menu');
+})
+
+const closeMenu = body.querySelector('.close-icon');
+closeMenu.addEventListener('click', () => {
+  body.classList.remove('menu');
+})
+
