c9149696afecdab9bfa3fc4b0af9b651753646a5
asanchy <sanchyclips@gmail.com>
2021-12-23 12:38:57 +0200

    Fix eslint errors

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,10 +3,9 @@ const menu = document.querySelector('.menu-icon');
 
 menu.addEventListener('click', () => {
   body.classList.add('menu');
-})
+});
 
 const closeMenu = body.querySelector('.close-icon');
 closeMenu.addEventListener('click', () => {
   body.classList.remove('menu');
-})
-
+});
